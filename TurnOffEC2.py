758014d9ba81e4a8f58b82ace4e8f8fe40d83a09
Fernando Pereiro <f.pereiro@ibermatica.com>
2017-07-16 19:52:29 +0200

    First commit

---
+++ TurnOffEC2.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+
+import boto3
+
+def toggle_instance(instance):
+    ec2 = boto3.resource("ec2", region_name="eu-west-1")
+    instance = ec2.Instance(instance["InstanceId"])
+    instance.stop()
+    print('Instance stopped.')
+
+def lambda_handler(event, context):
+    print('Looking for instances.')
+    ec2_client = boto3.client("ec2", region_name="eu-west-1")
+    description = ec2_client.describe_instances( Filters=[ { 'Name': 'tag-key', 
+        'Values': ['entorno'] },
+        { 'Name': 'tag-value', 'Values' : ['desarrollo'] }])
+
+    for reservation in description["Reservations"]:
+        for instance in reservation["Instances"]:
+            print('Instance: {}'.format(instance["InstanceId"]))
+            toggle_instance(instance)
